6025f09ffe1dd954deecf2cede85670ed57fe1cb
Rafael Petit <rpetit.dev@gmail.com>
2025-04-08 11:37:02 -0300

    feat: criando user admin ao iniciar

--- src/main/java/med/voli/api/ApiApplication.java
+++ src/main/java/med/voli/api/ApiApplication.java
@@ -1,8 +1,13 @@
 package med.voli.api;
 
+import med.voli.api.domain.user.User;
+import med.voli.api.domain.user.UserRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
@@ -12,4 +17,21 @@ public class ApiApplication {
 		SpringApplication.run(ApiApplication.class, args);
 	}
 
+
+	@Bean
+	public CommandLineRunner inicializarUsuarioAdmin(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+		return args -> {
+			if (userRepository.count() == 0) {
+				String senhaEncriptada = passwordEncoder.encode("admin");
+				User usuarioAdmin = new User("admin", senhaEncriptada);
+				userRepository.save(usuarioAdmin);
+
+				System.out.println("===========================================");
+				System.out.println("Usuário administrador criado com sucesso!");
+				System.out.println("Login: admin");
+				System.out.println("Senha: admin");
+				System.out.println("===========================================");
+			}
+		};
+	}
 }
